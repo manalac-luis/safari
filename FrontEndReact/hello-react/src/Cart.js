20e55074113c2a7a4f5a90bbd2e7e47913e725ac
lmanalac <lmanalac@mvesystems.com>
2018-08-19 20:38:40 -0700

    FrontEndReact D01

---
+++ FrontEndReact/hello-react/src/Cart.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Cart extends Component {
+  render() {
+
+    return (
+      <div>
+        <img src="shopping-cart-logo.svg" alt="cart" height="100px"/>
+      </div>
+    );
+  }
+}
+
+export default Cart;
---
+++ FrontEndReact/hello-react/src/Catalog.js
@@ -0,0 +1,53 @@
+import React,{Component} from 'react';
+import './Catalog.css';
+import ProductList from './ProductList';
+
+const style = {
+  color:'green',
+  textDecoration:'underline'
+}
+
+export class Catalog extends Component {
+
+
+  render() {
+    let title = 'Catalog';
+
+    return (
+      <div className='other-day'>
+        <h2>{title}</h2>
+        <ProductList />
+      </div>
+    );
+    {/*
+    let today = new Date().getDay();
+    let message;
+
+    if (today == 0){
+      message = <div className="sunday">Closed on Sunday</div>
+    } else {
+      message = <div className="otherday">Open Monday to Saturday</div>
+    }
+
+    return message;
+    */}
+    {/*
+    return(
+      <div>
+        <div>
+          <p></p>
+        </div>
+        <div className="form-row">
+          <label htmlFor="code" className="control-label" style={{color:'green'}}>Insert code</label>
+          <input type="text" className="form-control" name="code" />
+        </div>
+        <div className="form-row">
+          <button style ={style} type="submit" class="btn" >Send</button>
+        </div>
+      </div>
+    ); */
+    }
+  }
+}
+
+export default Catalog;
