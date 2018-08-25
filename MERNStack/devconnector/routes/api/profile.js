12b529d1d77feb3712d2f88881a63111fa0c0511
lmanalac <lmanalac@mvesystems.com>
2018-08-24 20:11:08 -0700

    MERNStack Chp04.3

--- MERNStack/devconnector/routes/api/profile.js
+++ MERNStack/devconnector/routes/api/profile.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+// Load User Model
+const User = require('../../models/User');
+
 
 // @route   GET api/profile/test
 // @desc    Test profile route
@@ -8,5 +17,31 @@ const router  = express.Router();
 
 router.get('/test',(req,res)=>res.json({msg:"Profile Works"})
 );
+
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      //.populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+
+
 
 module.exports=router;
