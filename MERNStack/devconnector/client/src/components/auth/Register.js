70f75fc08dd0fdf3ecb92e71c1cdfac86a5c1579
lmanalac <lmanalac@mvesystems.com>
2018-08-26 16:11:42 -0700

    MERNStack Chp08.4

--- MERNStack/devconnector/client/src/components/auth/Register.js
+++ MERNStack/devconnector/client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser} from '../../actions/authActions';
@@ -18,6 +18,13 @@ class Register extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.errors){
+      this.setState({errors:nextProps.errors});
+    }
+  }
+
   onChange(e) {
       this.setState({ [e.target.name]: e.target.value });
     }
@@ -31,7 +38,7 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registerUser(newUser);
+    this.props.registerUser(newUser,this.props.history);
 
     //console.log(newUser);
     //axios.post("/api/users/register",newUser)
@@ -44,7 +51,7 @@ class Register extends Component {
   render(){
 
     const {errors} = this.state;
-    const { user } = this.props.auth;
+
 
     return(
       <div className="register">
@@ -120,12 +127,14 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 }
 
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 })
 
-export default connect(mapStateToProps,{registerUser})(Register);
+export default connect(mapStateToProps,{registerUser})(withRouter(Register));
